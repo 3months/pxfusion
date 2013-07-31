f63278b34a0d52277559a46643bd1da968f6f789
Josh McArthur <joshua.mcarthur@gmail.com>
2013-08-01 11:58:41 +1200

    Test transaction class

--- spec/pxfusion/transaction_spec.rb
+++ spec/pxfusion/transaction_spec.rb
@@ -1,29 +1,26 @@
 require "spec_helper"
 
 describe PxFusion::Transaction do
-  subject { described_class.new(amount: 10.00, reference: Time.now.to_i) }
+  let(:timestamp) { Time.now.to_i }
 
-  describe ".initialize" do
+  subject { described_class.new(amount: "10.00", reference: timestamp) }
+
+  describe ".initialize", :vcr do
     context "normal attributes passed in" do
-      subject { described_class.new(amount: 10.00, reference: "12345") }
-      it { subject.amount.should eq 10.00 }
-      it { subject.reference.should eq "12345" }
+      it { subject.amount.should eq "10.00" }
+      it { subject.reference.should eq timestamp }
       it { subject.currency.should eq "NZD" }
+      it { subject.id.should_not be_nil }
+      it { subject.session_id.should_not be_nil }
     end
 
     context "missing attributes" do
-      it { expect { described_class.new(amount: 10.00) }.to raise_error(ArgumentError) }
+      it { expect { described_class.new(amount: "10.00") }.to raise_error(ArgumentError) }
     end
 
-    context "overriding globally-configured attributes" do
-      subject { described_class.new(amount: 10.00, reference: "12345", currency: "USD") }
-      it { subject.currency.should eq "USD" }
+    context "overriding globally-configured attributes", :vcr do
+      it { described_class.new(amount: "10.00", reference: "12345", currency: "USD").currency.should eq "USD" }
     end
   end
 
-  describe ".request_id!" do
-    VCR.use_cassette('get_transaction_id') do
-      it { expect { subject.send(:request_id!) }.to change(subject, :id) }
-    end
-  end
 end
