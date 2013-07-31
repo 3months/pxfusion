1e02615d6974e2fbdcb6b17db5333ca51a0a5d48
Josh McArthur <joshua.mcarthur@gmail.com>
2013-08-01 11:58:22 +1200

    Set correct options to quality XML tags and not log stuff

--- lib/pxfusion/client.rb
+++ lib/pxfusion/client.rb
@@ -2,6 +2,10 @@ require "savon"
 
 class PxFusion::Client < Savon::Client
   def initialize(options = {})
-    super(options.merge(wsdl: "#{PxFusion.endpoint}?wsdl"))
+    super(options.merge(
+      wsdl: PxFusion.endpoint + "?wsdl",
+      element_form_default: :qualified,
+      log: false
+    ))
   end
 end
