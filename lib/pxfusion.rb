9fe44b1a4691b0e547f1aa214e94d3fedf66401a
Josh McArthur <joshua.mcarthur@gmail.com>
2013-07-31 17:35:55 +1200

    Add default_currency configuration option

--- lib/pxfusion.rb
+++ lib/pxfusion.rb
@@ -1,15 +1,24 @@
 require "pxfusion/version"
 require "pxfusion/client"
+require "pxfusion/transaction"
 
 module PxFusion
   class << self
-    attr_writer :endpoint, :username, :password
+    attr_writer :endpoint, :username, :password, :default_currency
 
     [:endpoint, :username, :password].each do |required_attribute|
       define_method required_attribute do
         raise "#{required_attribute} must be set" if !instance_variable_get("@#{required_attribute}")
         instance_variable_get("@#{required_attribute}")
       end
+    end
+
+    def default_currency
+      @default_currency ||= "NZD"
+    end
+
+    def client
+      @client ||= Client.new
     end
   end
 end
