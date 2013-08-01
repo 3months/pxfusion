7a606de68d493d6efba27aced10ac6300456266d
Josh McArthur <joshua.mcarthur@gmail.com>
2013-08-01 15:31:57 +1200

    Spec the fetch method

--- spec/pxfusion/transaction_spec.rb
+++ spec/pxfusion/transaction_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe PxFusion::Transaction do
   let(:timestamp) { Time.now.to_i }
+  let(:complete_transaction_id) { "0000010016327641e4c03879868bd001" }
 
   subject(:transaction) { described_class.new(amount: "10.00", reference: timestamp) }
 
@@ -25,5 +26,24 @@ describe PxFusion::Transaction do
     subject { -> { transaction.reference = Time.now.to_i;transaction.generate_session_id! } }
     it { expect { subject.call }.to change(transaction, :session_id) }
     it { expect { subject.call }.to change(transaction, :id) }
+  end
+
+  describe ".fetch" do
+    context "transaction is complete", :vcr do
+      subject { PxFusion::Transaction.fetch(complete_transaction_id) }
+      it { subject.currency.should eq "NZD" }
+      it { subject.amount.should eq "1.00" }
+      it { subject.status.should eq PxFusion.statuses[:approved] }
+      it { subject.response.should_not be_empty }
+    end
+
+    context "transaction is incomplete", :vcr do
+      subject { transaction.reference = Time.now.to_i; transaction.generate_session_id! }
+      it { expect { PxFusion::Transaction.fetch(subject) }.to raise_error PxFusion::Transaction::NotFound }
+    end
+
+    context "transaction does not exist", :vcr do
+      it { expect { PxFusion::Transaction.fetch("1234") }.to raise_error(PxFusion::Transaction::NotFound) }
+    end
   end
 end
