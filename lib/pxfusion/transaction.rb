c6aa233e30879f62ceff42a3ad4193521dea84fe
Josh McArthur <joshua.mcarthur@gmail.com>
2013-09-30 12:14:11 +1300

    Revert "Fix EnableBillCard element name"
    This reverts commit af41a156c4ee2f51ba2b77151bbd442bd97bb625.

--- lib/pxfusion/transaction.rb
+++ lib/pxfusion/transaction.rb
@@ -76,7 +76,7 @@ class PxFusion::Transaction < OpenStruct
             txnRef: attributes[:reference],
             merchantReference: attributes[:reference],
             txnType: attributes[:type],
-            "EnableAddBillCard" => attributes[:token_billing]
+            enableAddBillCard: attributes[:token_billing]
           }
         }
       end
