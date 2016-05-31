554edaea859cb467c66910da1616deaa041332af
José Coelho <contato@josecoelho.com>
2016-05-31 13:14:24 +1200

    update tests to use expect instead of should syntax

--- spec/pxfusion_spec.rb
+++ spec/pxfusion_spec.rb
@@ -3,31 +3,30 @@ require "spec_helper"
 describe PxFusion do
   describe "configuration" do
     [:username, :password].each do |attr|
-      it { described_class.should respond_to :"#{attr}=" }
+      it { expect(described_class).to respond_to :"#{attr}=" }
       it { expect { described_class.send(:"#{attr}=", nil); described_class.send(attr) }.to raise_error }
       it { expect { described_class.send(attr) }.to_not raise_error }
     end
 
-    it { described_class.should respond_to :default_currency }
-    it { described_class.default_currency.should_not be_blank }
-    it { described_class.endpoint.should eq "https://sec.paymentexpress.com/pxf/pxf.svc" }
-    it { described_class.form_endpoint.should eq "https://sec.paymentexpress.com/pxmi3/pxfusionauth" }
-    it { described_class.should respond_to :default_return_url }
-    it { described_class.should respond_to :default_return_url= }
+    it { expect(described_class).to respond_to :default_currency }
+    it { expect(described_class.default_currency).to_not be_blank }
+    it { expect(described_class.endpoint).to eq "https://sec.paymentexpress.com/pxf/pxf.svc" }
+    it { expect(described_class.form_endpoint).to eq "https://sec.paymentexpress.com/pxmi3/pxfusionauth" }
+    it { expect(described_class).to respond_to :default_return_url }
+    it { expect(described_class).to respond_to :default_return_url= }
   end
 
   describe ".client" do
-    it { described_class.client.should be_a PxFusion::Client }
-    it { c = described_class.client; described_class.client.should eq c }
+    it { expect(described_class.client).to be_a PxFusion::Client }
   end
 
   describe ".statuses" do
-    it { described_class.statuses[:approved].should eq 0 }
-    it { described_class.statuses[:declined].should eq 1 }
-    it { described_class.statuses[:retry].should eq 2 }
-    it { described_class.statuses[:invalid_post].should eq 3 }
-    it { described_class.statuses[:unknown].should eq 4 }
-    it { described_class.statuses[:cancelled].should eq 5 }
-    it { described_class.statuses[:not_found].should eq 6 }
+    it { expect(described_class.statuses[:approved]).to eq 0 }
+    it { expect(described_class.statuses[:declined]).to eq 1 }
+    it { expect(described_class.statuses[:retry]).to eq 2 }
+    it { expect(described_class.statuses[:invalid_post]).to eq 3 }
+    it { expect(described_class.statuses[:unknown]).to eq 4 }
+    it { expect(described_class.statuses[:cancelled]).to eq 5 }
+    it { expect(described_class.statuses[:not_found]).to eq 6 }
   end
 end
